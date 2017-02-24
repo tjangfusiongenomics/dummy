a8a253d0b75ba47579397de32d6fa0dcce54c825
tjang <tjang@fusiongenomics.com>
2017-02-24 12:15:56 -0800

    first commit

---
+++ ClassLibrary1/Model/Activity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Model
+{
+    public class Activity
+    {
+        [Key]
+        [Display(Name = "Activity")]
+        public int ActivityId { get; set; }
+
+        [Display(Name = "Activity")]
+        public string ActivityDec { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        [Display(Name = "Date Created")]
+        [DataType(DataType.DateTime)]
+        public DateTime DateCreated { get; set; }
+
+        public List<Event> Event { get; set; }
+    }
+}
---
+++ ClassLibrary1/Model/Event.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Model
+{
+    public class Event
+    {
+        [Key]
+        public int EventId { get; set; }
+
+        [Display(Name = "Date & From time")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy hh:mm tt}")]
+        public DateTime FromDate { get; set; }
+
+        [Display(Name = "Date & To time")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy hh:mm tt}")]
+
+        [DataType(DataType.DateTime)]
+
+        public DateTime ToDate { get; set; }
+
+
+        [Display(Name = "Activity")]
+        public int ActivityId { get; set; }
+        public Activity Activity { get; set; }
+
+        [Display(Name = "Creator")]
+        public string Id { get; set; }
+
+
+    }
+}
